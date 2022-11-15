31c21c67e3b6d7111bd40cb92b23123afc5efe92
Gabriel Martins <gabriel-pms@hotmail.com>
2022-11-15 15:34:19 -0300

    rotas

---
+++ src/screens/Map/Map.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const Map: React.FC = () => {
+  return (
+    <View>
+            <Text>
+                Map
+            </Text>
+    </View>
+  );
+}
+
+export default Map;
